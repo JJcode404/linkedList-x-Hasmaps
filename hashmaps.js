3a6a9c3e03c58dcd397f95251ffd49aebdee44c5
thatboikham <khamjapher1@gmail.com>
2024-08-15 12:29:44 +0300

    feat: add a set function that add a pair

--- hashmaps.js
+++ hashmaps.js
@@ -10,8 +10,19 @@ function hashmaps(){
      
         return Math.abs(hashCode) % this.bucket.length;
       } 
+      this.set = (key,value) => {
+        const index = this.hash(key)
+        this.bucket[index] = this.bucket[index] || [];
+        this.bucket[index].push([key, value])
+        console.log(this.bucket);
+    };
     return{
         bucket,
+        set
     }
      
 };
+const hash = hashmaps();
+hash.set("thisKey","am a value");
+hash.set("thisKey","am a value neww");
+hash.set("mango","am a value");
