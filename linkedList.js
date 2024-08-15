fec292d578ca1b064a9c5b363a133bc40dff64e2
thatboikham <khamjapher1@gmail.com>
2024-08-15 21:16:43 +0300

    feat: attach methods to 'this'

--- linkedList.js
+++ linkedList.js
@@ -1,46 +1,48 @@
-function Node(data, next = null){
-    return{
+function Node(data, next = null) {
+    return {
         data,
         next
-    }
-};
-function Linkedlist(){
-    this.head = null
+    };
+}
+
+function Linkedlist() {
+    this.head = null;
     this.size = 0;
-    function AppendValue (data){
+
+    this.AppendValue = function (data) {
         const newNode = Node(data);
-        if(this.head == null){
+        if (this.head == null) {
             this.head = newNode;
-        }else{
-            let current = this.head
-            while(current.next !== null){
-                current = current.next
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
             }
             current.next = newNode;
-            // console.log(current.next)
         }
-        this.size++
-    }
-    function PrependValue(data){
+        this.size++;
+    };
+
+    this.PrependValue = function (data) {
         this.head = Node(data, this.head);
-        this.size++
-    }
-    function getTail(){
-        let tail = this.head
-        if(this.head == null){
+        this.size++;
+    };
+
+    this.getTail = function () {
+        let tail = this.head;
+        if (this.head == null) {
             return null;
         }
-        while(tail.next !== null){
-            tail = tail.next
+        while (tail.next !== null) {
+            tail = tail.next;
         }
         return tail;
-    }
-    function getDataAtIndex(index){
-  
+    };
+
+    this.getDataAtIndex = function (index) {
         let current = this.head;
         let count = 0;
         while (current !== null) {
-            console.log(current)
             if (count === index) {
                 return current.data;
             }
@@ -48,81 +50,73 @@ function Linkedlist(){
             current = current.next;
         }
         return null;
-    }
-    function popOutLastNode(){
-    const last = this.getTail(); 
-    let current = this.head;
-    this.size--;
-    if(this.head == null){
-        return null;
-    }
-    // console.log(last)
-    if(this.head.next === null){
-        this.head = null;
-        return null
-    }
-    while(current.next.next !== null){
-        current = current.next;
-    }
-    current.next = null;
-    }
-    function contains(value){
-        let current = this.head
-        while(current !== null){
-            if(current.data === value ){
-                console.log("true")
-                return true
-            }
-            current = current.next
+    };
+
+    this.popOutLastNode = function () {
+        const last = this.getTail();
+        let current = this.head;
+        this.size--;
+        if (this.head == null) {
+            return null;
         }
-        console.log("false")
+        if (this.head.next === null) {
+            this.head = null;
+            return null;
+        }
+        while (current.next.next !== null) {
+            current = current.next;
+        }
+        current.next = null;
+    };
+
+    this.contains = function (value) {
+        let current = this.head;
+        while (current !== null) {
+            if (current.data === value) {
+                return true;
+            }
+            current = current.next;
+        }
         return false;
-    }
-    function find(value){
-        let current = this.head
+    };
+
+    this.find = function (value) {
+        let current = this.head;
         let count = 0;
-        while(current !== null){
-            if(current.data === value ){
-                console.log(count)
-                return count
+        while (current !== null) {
+            if (current.data === value) {
+                return count;
             }
             count++;
-            current = current.next
+            current = current.next;
         }
-        console.log("null")
         return null;
-    }
-    return{
-        head,
-        size,
-        getTail,
-        AppendValue,
-        PrependValue,
-        getDataAtIndex,
-        popOutLastNode,
-        contains,
-        find
-    }
+    };
+
+    return this;
 }
-const ll =  Linkedlist();
-ll.AppendValue("amIndex0")
-ll.AppendValue("amIndex1")
-ll.AppendValue("amIndex2")
+
+const ll = new Linkedlist();
+ll.AppendValue("amIndex0");
+ll.AppendValue("amIndex1");
+ll.AppendValue("amIndex2");
 ll.contains("amIndexd1");
 ll.find("amIndex1");
 
-// ll.AppendValue("amIndex3")
-// ll.PrependValue("amIndex4")
-ll.popOutLastNode()
-ll.popOutLastNode()
-// ll.popOutLastNode()
-// ll.popOutLastNode()
+// ll.AppendValue("amIndex3");
+// ll.PrependValue("amIndex4");
+// ll.popOutLastNode();
+// ll.popOutLastNode();
+// ll.popOutLastNode();
+// ll.popOutLastNode();
 
+console.log(JSON.stringify(ll));
+console.log(ll.toString());
+console.log(ll.getDataAtIndex(0));
 
+const fromLinkedList = "am on the other side hahahah";
 
-
-console.log(
-    JSON.stringify(ll)
-  )
-  console.log(ll.toString());
-  console.log(ll.getDataAtIndex(0));
+export {
+    Linkedlist,
+    fromLinkedList
+};
