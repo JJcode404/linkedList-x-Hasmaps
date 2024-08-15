6c6178fc847ae0e415621aa405055545b3053eeb
thatboikham <khamjapher1@gmail.com>
2024-08-15 12:05:00 +0300

    feat: implement appendvalue that adds a linkedlist

--- linkedList.js
+++ linkedList.js
@@ -4,3 +4,21 @@ function Node(data, next = null){
         next
     }
 };
+function Linkedlist(){
+    this.head = null
+    this.size = 0;
+    function AppendValue (data){
+        const newNode = Node(data);
+        if(this.head == null){
+            this.head = newNode;
+        }else{
+            let current = this.head
+            while(current.next !== null){
+                current = current.next
+            }
+            current.next = newNode;
+            // console.log(current.next)
+        }
+        this.size++
+    }
+}
